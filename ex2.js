276f8c3189e654722215ba5aeff1fe51decd1483
Saad el kelkha <146880449+Saadelkelkha@users.noreply.github.com>
2024-05-28 21:29:55 +0100

    Add files via upload

---
+++ ex2.js
@@ -0,0 +1,16 @@
+function f1(){
+    var div = document.querySelector('div');
+    div.style.backgroundColor = "yellow";
+}
+function f2(){
+    var div = document.querySelector('div');
+    div.style.backgroundColor = "red";
+}
+function f3(){
+    var div = document.querySelector('div');
+    div.style.backgroundColor = "green";
+}
+function f4(){
+    var div = document.querySelector('div');
+    div.style.backgroundColor = "black";
+}
